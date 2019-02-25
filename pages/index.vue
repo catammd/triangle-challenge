702beea62cedf5a37e6b8cadda4d8b0a02a8bc38
Catalin Stefan Tatu <catalin.tatu@creuna.dk>
2019-02-25 22:57:53 +0100

    Form component WIP

--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,82 @@
 <template>
   <section class="container">
     <div>
-      <logo />
       <h1 class="title">
         Triangle-challenge
       </h1>
-      <form id="triangle-form" action="#">
-        <input-field 
-          v-for="(side, index) in sides"
-          :key="index"
-          :name="side.name"
-          :placeholder-text="side.placeholderText"
-          :label="side.label"
-        />
+      <form id="triangle-form" @submit.prevent="validateForm">
+        <fieldset>
+          <label for="side-A">A:</label>
+          <input 
+            id="side-A" 
+            v-model="sideA" 
+            type="number" 
+            name="side-A" 
+            autocomplete="off"
+            placeholder="Enter side A length" 
+          >
+        </fieldset>
+        <fieldset>
+          <label for="side-B">B:</label>
+          <input 
+            id="side-B"
+            v-model="sideB"  
+            type="number" 
+            name="side-B" 
+            autocomplete="off" 
+            placeholder="Enter side B length" 
+          >
+        </fieldset>
+        <fieldset>
+          <label for="side-C">C:</label>
+          <input 
+            id="side-C" 
+            v-model="sideC" 
+            type="number" 
+            name="side-C" 
+            autocomplete="off" 
+            placeholder="Enter side C length" 
+          >
+        </fieldset>
+        <fieldset>
+          <button class="ts-primary" type="submit">
+            <span>Generate triangle</span>
+          </button>
+        </fieldset>
       </form>  
     </div>
   </section>
 </template>
 
 <script>
-import inputField from '@/components/InputField'
 export default {
-  components: {
-    inputField
-  },
   data() {
     return {
-      sides: [
-        {
-          name: 'side-A',
-          label: 'A:',
-          placeholderText: 'Enter side A length'
-        },
-        {
-          name: 'side-B',
-          label: 'B:',
-          placeholderText: 'Enter side B length'
-        },
-        {
-          name: 'side-C',
-          label: 'C:',
-          placeholderText: 'Enter side C length'
-        }
-      ]
+      sideA: '',
+      sideB: '',
+      sideC: ''
+    }
+  },
+  computed: {
+    noValidInputs: function() {
+      return (
+        this.isNumeric(this.sideA) === false ||
+        this.sideA < 1 ||
+        this.isNumeric(this.sideB) === false ||
+        this.sideB < 1 ||
+        this.isNumeric(this.sideC) === false ||
+        this.sideC < 1
+      )
+    }
+  },
+  methods: {
+    isNumeric: function(n) {
+      return !isNaN(parseFloat(n)) && isFinite(n)
+    },
+    validateForm: function(e) {
+      // eslint-disable-next-line no-console
+      console.log('are inputs valid', !this.noValidInputs)
+      e.preventDefault()
     }
   }
 }
